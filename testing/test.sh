f6aaf3a39b89b5e286c60dac8d96d76b9f757275
csahraoui <c.sahraoui@gmail.com>
2022-08-10 10:18:38 +0200

    updated testing script

--- testing/test.sh
+++ testing/test.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 if [[ $# -ge 1 ]]; then
-    for ((i=1;i<=100;i++)); do echo $i; curl -s --write-out "time_total: %{time_total} // http_code: %{http_code} " $1 | grep -o 'PRIMARY\|SECONDARY'; echo -n " @ "; date +"%Y-%m-%d %H:%M:%S"; sleep 1; done
+
+    for ((i=1;i<=100;i++));
+    do
+    status_code=`curl --silent --connect-timeout 8 --output /dev/null $1 -I -w "%{http_code}\n"`
+    if [[ "$status_code" -ge 500 ]] ; then
+        echo "$i,DOWN,$(date +"%Y-%m-%d %H:%M:%S")"; sleep 1; 
+    else
+        echo "$i,$(curl -s $1 | grep -o 'PRIMARY\|SECONDARY'),$(date +"%Y-%m-%d %H:%M:%S")"; sleep 1; 
+
+    fi
+    done
     exit $?
 else
     echo 1>&2 "###############################################################################"
