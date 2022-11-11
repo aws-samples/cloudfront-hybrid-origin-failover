31c09b9eebf0cc63a2acc2b1d95e66574e51a781
csahraoui <c.sahraoui@gmail.com>
2022-11-11 23:38:12 +0100

    included TTFB to the testing script

--- testing/test.sh
+++ testing/test.sh
@@ -3,11 +3,13 @@ if [[ $# -ge 1 ]]; then
 
     for ((i=1;i<=300;i++));
     do
-    status_code=`curl --silent --connect-timeout 8 --output /dev/null $1 -I -w "%{http_code}\n"`
+    response=`curl --silent --connect-timeout 8 --output /dev/null "https://d1hz8aptdjgovk.cloudfront.net" -I -w "%{http_code} %{time_starttransfer}\n"`
+    status_code=` echo $response | awk '{print $1}'`
+    TTFB=` echo $response | awk '{print $2}'`
     if [[ "$status_code" -ge 500 ]] ; then
         echo "$i,DOWN,$(date +"%Y-%m-%d %H:%M:%S")"; sleep 1; 
     else
-        echo "$i,$(curl -s $1 | grep -o 'PRIMARY\|SECONDARY'),$(date +"%Y-%m-%d %H:%M:%S")"; sleep 1; 
+        echo "$i,$(curl -s $1 | grep -o 'PRIMARY\|SECONDARY'),$(date +"%Y-%m-%d %H:%M:%S"),$TTFB"; sleep 1; 
 
     fi
     done
