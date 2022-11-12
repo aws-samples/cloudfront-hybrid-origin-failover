faf8188c0566d2d7f87baf5b7a804322d73818a1
csahraoui <c.sahraoui@gmail.com>
2022-11-12 01:05:54 +0100

    test.sh bug fix

--- testing/test.sh
+++ testing/test.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 if [[ $# -ge 1 ]]; then
-
+    echo "---------------------------------------------"
+    echo "req# | status | timestamp | statusCode | TTFB"
+    echo "---------------------------------------------"
     for ((i=1;i<=300;i++));
     do
-    response=`curl --silent --connect-timeout 8 --output /dev/null "https://d1hz8aptdjgovk.cloudfront.net" -I -w "%{http_code} %{time_starttransfer}\n"`
-    status_code=` echo $response | awk '{print $1}'`
-    TTFB=` echo $response | awk '{print $2}'`
+    response=`curl --silent --connect-timeout 8 $1 -w "%{http_code} %{time_starttransfer}\n"`
+    status_code=`echo $response | awk '{print $9}'`
+    TTFB=`echo $response | awk '{print $10}'`
+    respBody=`echo $response | awk '{print $5}'`
     if [[ "$status_code" -ge 500 ]] ; then
-        echo "$i,DOWN,$(date +"%Y-%m-%d %H:%M:%S")"; sleep 1; 
+        echo "$i,DOWN,$(date +"%Y-%m-%d %H:%M:%S"),$status_code"; sleep 1; 
     else
-        echo "$i,$(curl -s $1 | grep -o 'PRIMARY\|SECONDARY'),$(date +"%Y-%m-%d %H:%M:%S"),$TTFB"; sleep 1; 
+        echo "$i,$respBody,$(date +"%Y-%m-%d %H:%M:%S"),$status_code,$TTFB"; sleep 1; 
 
     fi
     done
